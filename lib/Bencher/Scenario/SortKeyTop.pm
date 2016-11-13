f1f101b89863cf512e632f13bc53536a0f591cc0
perlancar (@netbook-zenbook-ux305) <perlancar@gmail.com>
2016-11-13 14:35:49 +0700

    Initial commit

---
+++ lib/Bencher/Scenario/SortKeyTop.pm
@@ -0,0 +1,40 @@
+package Bencher::Scenario::SortKeyTop;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+our $scenario = {
+    summary => 'Benchmark Sort::Key::Top',
+    participants => [
+        {
+            name => 'sort',
+            summary => "Perl's sort() builtin",
+            code_template=>'state $elems=<elems>; my @sorted = sort @$elems; splice @sorted, <n>',
+            result_is_list => 1,
+        },
+        {
+            name => 'Sort::Key::Top',
+            fcall_template => 'Sort::Key::Top::nkeytopsort(sub { $_ }, <n>, @{<elems>})',
+            result_is_list => 1,
+        },
+        {
+            name => 'Sort::Key::Top::PP',
+            fcall_template => 'Sort::Key::Top::PP::nkeytopsort(sub { $_ }, <n>, @{<elems>})',
+            result_is_list => 1,
+        },
+    ],
+    datasets => [
+        {name=>'elems=10 , n=5'    , args=>{elems=>[reverse 1..10]  , n=>  5}},
+        {name=>'elems=100, n=10'   , args=>{elems=>[reverse 1..100] , n=> 10}},
+        {name=>'elems=1000, n=10'  , args=>{elems=>[reverse 1..1000], n=> 10}},
+    ],
+};
+
+1;
+# ABSTRACT:
+
+=head1 SEE ALSO
